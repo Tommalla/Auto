932029b74e61eec40413c8432b94269f2ebd6658
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2015-03-30 12:49:23 +0200

    Fixed validation in RunAuto.

--- Auto/RunAuto.hs
+++ Auto/RunAuto.hs
@@ -1,19 +1,16 @@
 import Data.Char
 import Data.Maybe
-import System.Exit
 import System.IO
 import Text.Read
 import Auto
 
 
-parseTrans :: [String] -> Maybe [(Maybe Int, Char, [Maybe Int])]  -- TODO FIXME get rid of the maybes!
-parseTrans (state:chars:rest) = if (any (not . isUpper) chars) then Nothing else (Just (map (\c -> (readMaybe state, c, map (readMaybe) rest)) chars))
+parseTrans :: [String] -> Maybe [(Int, Char, [Int])]
+parseTrans (state:chars:rest) = if ((any (not . isUpper) chars) || (isNothing maybeState) || (any (isNothing) maybeRest)) then Nothing else (Just (map (\c -> (fromJust maybeState, c, map (fromJust) maybeRest)) chars))
+    where
+        maybeState = readMaybe state
+        maybeRest = map (readMaybe) rest
 parseTrans _ = Nothing
-
-
-maybeList :: Maybe [a] -> [Maybe a]
-maybeList (Just l) = map (\elem -> Just elem) l
-maybeList Nothing = [Nothing]
 
 
 concatMaybe :: [Maybe [a]] -> Maybe [a]
@@ -23,7 +20,7 @@ concatMaybe l = if ((length filtered) /= (length l)) then Nothing else (Just (co
 
 
 main = do
-    handle <- openFile "test2.in" ReadMode
+    handle <- openFile "test1.in" ReadMode
     let hGetLine' = hGetLine handle
     line <- hGetLine'
     let numStates = readMaybe line :: Maybe Int
@@ -35,8 +32,10 @@ main = do
     input <- hGetContents handle
     let (word:inputLines) = (reverse . lines) input
     let transitions = concatMaybe (map (parseTrans . words) inputLines)
-    if ((isNothing transitions) || (isNothing numStates) || (isNothing initStates) || (isNothing acceptingStates)) then (putStrLn "BAD INPUT" >> exitFailure) else return ()
     
-    let auto = fromLists (maybeList (Just [1.. (fromJust numStates)])) (maybeList initStates) (maybeList acceptingStates) (fromJust transitions)
-    if (any (not. isUpper) word) then (putStrLn "BAD INPUT" >> exitFailure) else return ()
-    print (accepts auto word)
+    if ((isNothing transitions) || (isNothing numStates) || (isNothing initStates) || (isNothing acceptingStates) || (any (not . isUpper) word)) 
+       then 
+            putStrLn "BAD INPUT" 
+       else do
+            let auto = fromLists [1.. (fromJust numStates)] (fromJust initStates) (fromJust acceptingStates) (fromJust transitions)
+            print (accepts auto word)
